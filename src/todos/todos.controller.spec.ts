73fc78b248523743c8747807ec0ba79938b70f2c
yongjoon <yongjoon@bearrobotics.ai>
2021-03-07 22:59:43 +0900

    Add new database for testing and fixed controller test

--- src/todos/todos.controller.spec.ts
+++ src/todos/todos.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Todo } from './todo.entity';
 import { TodosController } from './todos.controller';
@@ -8,21 +8,39 @@ import { TodosService } from './todos.service';
 describe('TodosController Test', () => {
   let todosController: TodosController;
   let todosService: TodosService;
+  let repository: Repository<Todo>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      controllers: [TodosController],
-      providers: [
-        TodosService,
-        {
-          provide: getRepositoryToken(Todo),
-          useValue: {},
-        },
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: process.env.DB_PASSWORD,
+          database: 'pomodoro_test',
+          entities: ['src/**/*.entity{.ts,.js}'],
+          synchronize: false,
+          keepConnectionAlive: true,
+        }),
+        TypeOrmModule.forFeature([Todo]),
       ],
+      controllers: [TodosController],
+      providers: [TodosService],
     }).compile();
+    repository = moduleRef.get('TodoRepository');
 
     todosService = moduleRef.get<TodosService>(TodosService);
     todosController = moduleRef.get<TodosController>(TodosController);
+  });
+
+  afterEach(async () => {
+    await repository.query(`DELETE from todo;`);
+  });
+
+  afterAll(async () => {
+    await repository.manager.connection.close();
   });
 
   describe('findAll', () => {
@@ -47,20 +65,30 @@ describe('TodosController Test', () => {
 
   describe('patch', () => {
     it('should edit one of the todos', async () => {
-      const result: Todo = {
-        id: 1,
-        title: 'title',
-        sprintTotal: 2,
-        sprintDone: 1,
+      const todo = await todosController.create({
+        title: 'hello world',
+        sprintDone: 3,
+        sprintTotal: 5,
         todoDone: false,
-      };
+      });
+      const editedTodo = await todosController.editOne({
+        id: todo.id,
+        title: 'see you later',
+      });
+      expect(editedTodo.title).toBe('see you later');
+    });
+  });
 
-      jest
-        .spyOn(todosService, 'editOne')
-        .mockImplementationOnce(async () => result);
-      expect(
-        await todosController.editOne({ id: 1, title: 'hello world' }),
-      ).toBe(result);
+  describe('create', () => {
+    it('should create a todo', async () => {
+      const newTodo = await todosController.create({
+        title: 'hello world',
+        sprintDone: 3,
+        sprintTotal: 5,
+        todoDone: false,
+      });
+      const todo = await repository.findOne(newTodo.id);
+      expect(todo.title).toBe('hello world');
     });
   });
 });
