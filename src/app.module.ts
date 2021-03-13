2c3dcf3c22bf363a1f03d4a1346bd7aecf9b7e8f
yongjoon <yongjoon@bearrobotics.ai>
2021-03-13 22:15:11 +0900

    Add @nestjs/config to load dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -7,9 +7,8 @@ import { OAuth2Strategy, VerifyFunction } from 'passport-google-oauth';
 export class GoogleStrategy extends PassportStrategy(OAuth2Strategy) {
   constructor(private authService: AuthService) {
     super({
-      clientID:
-        '435036087399-r9a9oocu8lraebtuqemen1gt6ur893sb.apps.googleusercontent.com',
-      clientSecret: 'wk8Hy3pJSyLz6BimYzaT5fEL',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/auth/google/callback',
       scope: ['email', 'profile'],
     });
@@ -21,8 +20,8 @@ export class GoogleStrategy extends PassportStrategy(OAuth2Strategy) {
     profile: any,
     cb: VerifyFunction,
   ): Promise<any> {
-    const { id, name, emails, photos } = profile;
     await this.authService.validateOAuthUser(profile, 'google');
+    const { id, name, emails, photos } = profile;
     const user = {
       id,
       email: emails[0].value,
