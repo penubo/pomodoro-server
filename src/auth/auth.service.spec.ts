af85774c4c7055f1309ff77a2629c9e2000add66
yongjoon <yongjoon@bearrobotics.ai>
2021-03-14 15:29:35 +0900

    Fix auth service to using mock

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,10 +1,13 @@
 import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from '../users/users.service';
 import { Repository } from 'typeorm';
 import { User } from '../users/user.entity';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { GoogleStrategy } from './google.strategy';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -27,8 +30,9 @@ describe('AuthService', () => {
       providers: [
         {
           provide: getRepositoryToken(User),
-          useValue: mockUsersRepository,
+          useFactory: mockUsersRepository,
         },
+        UsersService,
         AuthService,
       ],
     }).compile();
